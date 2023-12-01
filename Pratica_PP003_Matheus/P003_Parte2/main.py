f44b69ff0f6d074616cfffe6ff962cc172268462
mfnswdev <engenharia.mfn@gmail.com>
2023-12-01 06:49:36 -0300

    Add P003 branch dev Matheus

---
+++ Pratica_PP003_Matheus/P003_Parte2/main.py
@@ -0,0 +1,40 @@
+# main.py
+import os
+from empresa import *
+
+def main():
+    # arquivo_empregados = "./dados_empregados.txt"
+    caminho_arquivo = os.path.abspath("dados_empregados.txt")
+
+    empregados = ler_dados_arquivo(caminho_arquivo)
+
+    while True:
+        print("\n=== Empresa ===")
+        print("1. Reajustar salários em 10%")
+        print("2. Listar informações dos empregados")
+        print("0. Sair")
+
+        opcao = input("Escolha uma opção: ")
+
+        if opcao == "1":
+            reajusta_dez_porcento(empregados)
+            print("Salários reajustados em 10%.")
+        elif opcao == "2":
+            listar_empregados(empregados)
+        elif opcao == "0":
+            print("Saindo do programa. Até mais!")
+            break
+        else:
+            print("Opção inválida. Tente novamente.")
+
+def listar_empregados(empregados):
+    if not empregados:
+        print("Nenhum empregado cadastrado.")
+        return
+
+    print("Lista de empregados:")
+    for i, empregado in enumerate(empregados, start=1):
+        print(f"{i}. Nome: {empregado['nome']} {empregado['sobrenome']}, Ano de Nascimento: {empregado['ano_nascimento']}, RG: {empregado['rg']}, Ano de Admissão: {empregado['ano_admissao']}, Salário: R${empregado['salario']:.2f}")
+
+if __name__ == "__main__":
+    main()
