e4d927d19049e443721263f39025895a17d4b154
mfnswdev <engenharia.mfn@gmail.com>
2023-12-02 22:37:21 -0300

    Add estrutura de classes implementadas e modularizadas parte 1

---
+++ Pratica_PP004_Matheus/class_AnaliseDados.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+
+class AnaliseDados(ABC): 
+
+    @abstractmethod
+    def __init__(self, tipoDeDados):
+        self.__tipoDeDados = tipoDeDados
+
+    @abstractmethod
+    def entradaDeDados(self):
+        pass
+
+    @abstractmethod
+    def mostraMediana(self):
+        pass
+    
+    @abstractmethod
+    def mostraMenor(self):
+        pass
+
+    @abstractmethod
+    def mostraMaior(self):
+        pass
